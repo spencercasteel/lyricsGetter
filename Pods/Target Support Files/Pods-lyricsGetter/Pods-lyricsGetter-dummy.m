8e9a0422c5ceaa8c21cac2bb9eccb85af229e827
Spencer Casteel <spencercasteel@gmail.com>
2018-10-23 14:06:40 -0500

    all the code to make it work and pictures

---
+++ Pods/Target Support Files/Pods-lyricsGetter/Pods-lyricsGetter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lyricsGetter : NSObject
+@end
+@implementation PodsDummy_Pods_lyricsGetter
+@end
--- lyricsGetter/ViewController.swift
+++ lyricsGetter/ViewController.swift
@@ -7,14 +7,70 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var artistTextFeild: UITextField!
+    @IBOutlet weak var songTextFeild: UITextField!
+    @IBOutlet weak var lyricsTextView: UITextView!
+    
+    //the base URL for the lyrics API, aka the point where we connect to it
+    let lyricsAPIBaseURL = "https://api.lyrics.ovh/v1"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       artistTextFeild.delegate = self
+        songTextFeild.delegate = self
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    @IBAction func submitButttonTapped(_ sender: Any) {
+        guard let artistName = artistTextFeild.text, let songTitle = songTextFeild.text else {
+            return
+        }
+        
+        //since we cant use spaces in our URL, we need to replace them with a +
+        let artistNameURLComponent = artistName.replacingOccurrences(of: " ", with: "+")
+        
+        let songTitleURLComponent = songTitle.replacingOccurrences(of: " ", with: "+")
+        
+        //Full URL for the request we will make to the API
+        let requestURL = "\(lyricsAPIBaseURL)/\(artistNameURLComponent)/\(songTitleURLComponent)"
+        print(requestURL)
+        
+        //we are going to use alamoFire to create an
+        let request = Alamofire.request(requestURL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil)
+        
+        //now we need to actully make our request
+        request.responseJSON { response in
+            //we switch based on response result, which can be either success or failure
+            switch response.result {
+            case .success(let Value):
+               //in the case of success, the request has succeeded, and we've gotten some data back
+                print("Success!")
+                
+                let json = JSON(Value)
+                
+                self.lyricsTextView.text = json["lyrics"].stringValue
+            case .failure(let Error):
+                //in the case of failure, the request has failed and we've gotten an error back
+                print("Error. :(")
+                print(Error.localizedDescription)
+                
+            }
+        }
+        artistTextFeild.text = ""
+        songTextFeild.text = ""
+    }
 }
 

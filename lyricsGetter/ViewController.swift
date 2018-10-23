2089a4751c2ebeaa2c14620813b8b06ab74d500e
Spencer Casteel <spencercasteel@gmail.com>
2018-10-23 10:22:50 -0500

    Initial Commit

---
+++ lyricsGetter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lyricsGetter
+//
+//  Created by Spencer Casteel on 10/23/18.
+//  Copyright © 2018 Spencer Casteel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
